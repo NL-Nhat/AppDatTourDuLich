6ef2aca9c0d9b7095d29f2f4e840f0597d4ff389
nguyenphuoc0768 <nguyenphuoc0768@gmail.com>
2025-11-24 21:18:05 +0700

    Man hinh dang ky

---
+++ app/src/main/java/com/example/apptravel/models/DangKyActivity.java
@@ -0,0 +1,15 @@
+package com.example.apptravel.models;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apptravel.R;
+
+public class DangKyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangky);
+    }
+}
