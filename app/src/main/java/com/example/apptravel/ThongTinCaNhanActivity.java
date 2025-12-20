98c43fde0b444255655d56cfcf2c3ab8ab37569b
nguyenphuoc0768 <nguyenphuoc0768@gmail.com>
2025-12-20 11:57:27 +0700

    cho chon lich

--- app/src/main/java/com/example/apptravel/ThongTinCaNhanActivity.java
+++ app/src/main/java/com/example/apptravel/ThongTinCaNhanActivity.java
@@ -1,17 +1,25 @@
 package com.example.apptravel;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class ThongTinCaNhanActivity extends AppCompatActivity {
 
     private ImageView btnBack;
     private Button btnHuy;
+
+    private EditText txtngaysinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +44,34 @@ public class ThongTinCaNhanActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        txtngaysinh = findViewById(R.id.edit_birthday);
+
+        txtngaysinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ngay();
+            }
+        });
+    }
+
+    private void Ngay() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        String formattedDate = String.format(Locale.getDefault(), "%02d/%02d/%d", selectedDay, selectedMonth + 1, selectedYear);
+                        txtngaysinh.setText(formattedDate);
+                    }
+                },
+                year, month, day
+        );
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/apptravel/TrangChinhFragment.java
+++ app/src/main/java/com/example/apptravel/TrangChinhFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 public class TrangChinhFragment extends Fragment {
 
     private TextView txtXemThem;
+    private TextView txttien;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,6 +30,7 @@ public class TrangChinhFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        txttien = (TextView) view.findViewById(R.id.txtTien);
 
         return view;
     }
